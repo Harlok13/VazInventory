a736700b60ae4278b0ecda66567e6cb5e5ed8d02
Harlok <holmik90@yandex.ru>
2023-09-06 23:07:05 +0400

    feat: add Program.cs

---
+++ Inventory/src/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace Inventory
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            using (var context = new Context())
+            {
+
+            }
+        }
+    }
+}
