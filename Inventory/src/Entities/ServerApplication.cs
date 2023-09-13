b5c3e5ab93265837a39a65916585f63a8446b74d
Harlok <holmik90@yandex.ru>
2023-09-13 14:46:43 +0400

    feat: add inheritance from IEntities

--- Inventory/src/Entities/ServerApplication.cs
+++ Inventory/src/Entities/ServerApplication.cs
@@ -1,11 +1,11 @@
 namespace Inventory.Entities;
 
-public class ServerApplication
+public class ServerApplication : IEntities
 {
     public int Id { get; set; }
 
-    [Column(TypeName = "VARCHAR"), StringLength(32), Required]
-    public string Name { get; set; } = null!;
+    [Column(TypeName = "VARCHAR"), StringLength(32)]
+    public string? Name { get; set; }
     public string? Version { get; set; }
 
     public ICollection<Server> Servers { get; set; } = null!;
