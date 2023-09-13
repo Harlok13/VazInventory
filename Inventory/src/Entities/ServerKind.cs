69d0ed0641ffd8df780848a449b7bce2d65f8e1a
Harlok <holmik90@yandex.ru>
2023-09-13 14:47:00 +0400

    feat: add inheritance from IEntities

--- Inventory/src/Entities/ServerKind.cs
+++ Inventory/src/Entities/ServerKind.cs
@@ -2,13 +2,13 @@ using Microsoft.EntityFrameworkCore;
 
 namespace Inventory.Entities;
 
-public class ServerKind
+public class ServerKind : IEntities
 {
     public int Id { get; set; }
     
     [Comment("Example: API")]
-    [Column(TypeName = "VARCHAR"), StringLength(16), Required]
-    public string KindName { get; set; } = null!;
+    [Column(TypeName = "VARCHAR"), StringLength(16)]
+    public string? KindName { get; set; }
 
     public ICollection<Server> Servers { get; set; } = null!;
 }
