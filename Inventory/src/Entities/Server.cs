b4e12c63ec4b4a0b2e7f095292bb8b927e2b70f8
Harlok <holmik90@yandex.ru>
2023-09-13 14:46:17 +0400

    feat: add inheritance from IEntities

--- Inventory/src/Entities/Server.cs
+++ Inventory/src/Entities/Server.cs
@@ -2,7 +2,7 @@ using Microsoft.EntityFrameworkCore;
 
 namespace Inventory.Entities;
 
-public class Server
+public class Server : IEntities
 {
     public int Id { get; set; }
 
@@ -11,17 +11,20 @@ public class Server
     public string Domain { get; set; } = null!;
     
     public int ContourId { get; set; }
-    public Contour Contour { get; set; } = null!;
+    public Contour? Contour { get; set; }
     
-    public int ServerOsId { get; set; }
-    public ServerOs ServerOs { get; set; } = null!;
+    public string? ServerOsId { get; set; }
+    public ServerOs? ServerOs { get; set; }
     
-    public int ServerApplicationId { get; set; }
-    public ServerApplication ServerApplication { get; set; } = null!;
+    public int? ServerApplicationId { get; set; }
+    public ServerApplication? ServerApplication { get; set; }
 
     public int ServerKindId { get; set; }
-    public ServerKind ServerKind { get; set; } = null!;
+    public ServerKind? ServerKind { get; set; }
     
-    public int LocationId { get; set; }
-    public Location Location { get; set; } = null!;
+    public int? LocationId { get; set; }
+    public Location? Location { get; set; } 
+    
+    public int? InformationSystemId { get; set; }
+    public InformationSystem? InformationSystem { get; set; } 
 }
