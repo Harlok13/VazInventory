940f55993b4c9975a91bd648f703ddf2262b37e3
Harlok <holmik90@yandex.ru>
2023-09-14 11:21:25 +0400

    chore: del comment

--- Inventory/src/Data/Entities/Contour.cs
+++ Inventory/src/Data/Entities/Contour.cs
@@ -3,8 +3,7 @@ namespace Inventory.Data.Entities;
 public class Contour : IEntities
 {
     public int Id { get; set; }
-
-    // [Key]
+    
     [Column(TypeName = "VARCHAR"), StringLength(32), Required]
     public string? Name { get; set; }
 
