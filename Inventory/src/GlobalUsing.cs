99a9a2c8cc900c1dfe1ecad6ce30ec3addee9e14
Harlok <holmik90@yandex.ru>
2023-09-14 11:18:10 +0400

    chore: add Settings

--- Inventory/src/GlobalUsing.cs
+++ Inventory/src/GlobalUsing.cs
@@ -2,4 +2,6 @@ global using static System.Console;
 global using System.ComponentModel.DataAnnotations;
 global using System.ComponentModel.DataAnnotations.Schema;
 
+global using static Inventory.Settings;
+
 namespace Inventory;
