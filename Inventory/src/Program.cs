617b7315d519f18d0d6070e6c3a8ee840a4d02dd
Harlok <holmik90@yandex.ru>
2023-09-23 23:57:17 +0400

    ref: separation of logic

--- Inventory/src/Program.cs
+++ Inventory/src/Program.cs
@@ -1,29 +1,44 @@
-using System.Collections.Immutable;
-using Inventory.Data.Context;
-using Inventory.ExcelProcessing;
+using Npgsql;
 
-namespace Inventory
+using Inventory.BL.FileProcessing;
+using Inventory.DAL.Cache;
+using Inventory.DAL.Context;
+using Inventory.DAL.ExcelToDatabase;
+
+namespace Inventory;
+
+class Program
 {
-    class Program
+    public static async Task Main(string[] args)
     {
-        public static async Task Main(string[] args)
+        try
         {
             await using var context = new InventoryContext(DbDSN);
-
             await context.CheckDbConnectionOrThrowAsync();
 
-            var files = Directory.GetFiles(PathToXlsxFiles);
+            var fileProcessing = new FileProcessing();
+            var cacheData = new CacheData();
 
-            var filesList = files.Where(f => f.EndsWith("xlsx")).ToImmutableArray();
-
-            foreach (var file in filesList)
-            {
-                var xlsx = new XlsxProcessing(PathToXlsxFiles, file);
-
-                await xlsx.SetServersAsync(context, InventoryContext.CacheDict);
-            }
+            await fileProcessing.StartAsync(context, new ExcelToDatabase(), cacheData, DirectoriesForParse);
 
             await context.SaveChangesAsync();
+                
+            WriteLine("The program has successfully completed its work.");
+        }
+        catch (NpgsqlException ex)
+        {
+            WriteLine(ex.Message);
+            throw new Exception("Connection refused! Check the connection string or make sure" +
+                                " that the server is running.");
+        }
+        catch (DirectoryNotFoundException ex)
+        {
+            WriteLine("Make sure that you have correctly specified the path to the directory with the files!" +
+                      $"\n{ex.Message}");
+        }
+        finally
+        {
+            WriteLine("The application has terminated.");
         }
     }
 }
