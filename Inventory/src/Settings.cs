7a2151e880c1d9236842f48e301fabb8ac186d5b
Harlok <holmik90@yandex.ru>
2023-09-23 23:43:18 +0400

    feat: add Lanit settings

--- Inventory/src/Settings.cs
+++ Inventory/src/Settings.cs
@@ -1,3 +1,4 @@
+using System.Collections.Immutable;
 using System.Configuration;
 
 namespace Inventory;
@@ -9,7 +10,7 @@ public static class Settings
 
     public static readonly bool RecreatingDb = true;
     
-    public static readonly string PathToXlsxFiles = ConfigurationManager.AppSettings.Get("PathToXlsxFiles")
+    public static readonly string PathToInventoryXlsxFiles = ConfigurationManager.AppSettings.Get("PathToInventoryXlsxFiles")
                                                     ?? throw new Exception("Path to xlsx files is missing!");
 
     public static readonly int WorkSheetNum = 0;
@@ -40,4 +41,27 @@ public static class Settings
     public static readonly int ServerApplicationVersionCol = 6;
 
     public static readonly int ServerLocationCol = 7;
+    
+    // ________________________________________________________________________________________________
+
+    public static readonly string DbLanitDSN = ConfigurationManager.AppSettings.Get("DbLanitDSN")
+                                               ?? throw new Exception("Db Lanit connection string is missing!");
+    
+    public static readonly int LanitCodeCol = 0;
+    public static readonly int LanitSystemNameCol = 1;
+    public static readonly int LanitDomainCol = 3;
+    public static readonly int LanitServersKindCol = 4;
+    public static readonly int LanitIntegrationsCol = 5;
+
+    public static readonly string PathToLanitXlsxFile = ConfigurationManager.AppSettings.Get("PathToLanitXlsxFile")
+                                                        ?? throw new Exception("Path to Lanit xlsx file is missing!");
+
+    public static readonly string LanitFileName = "LanitCompare.xlsx";
+    
+    public static readonly int StartRow = 1;
+
+    public static readonly List<string> DirectoriesForParse = new()
+        { PathToInventoryXlsxFiles, PathToLanitXlsxFile };
+
+    public static string Language = new string[] { "Ru", "En" }[0];
 }
