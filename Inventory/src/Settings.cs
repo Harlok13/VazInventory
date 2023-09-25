a76aa142ed76e44d7b1af89e99cf241da3c5d350
Harlok <holmik90@yandex.ru>
2023-09-25 21:32:20 +0400

    fix: fix fields modifier

--- Inventory/src/Settings.cs
+++ Inventory/src/Settings.cs
@@ -1,4 +1,3 @@
-using System.Collections.Immutable;
 using System.Configuration;
 
 namespace Inventory;
@@ -8,12 +7,12 @@ public static class Settings
     public static readonly string DbDSN = ConfigurationManager.AppSettings.Get("DbDSN")
                                           ?? throw new Exception("Db connection string is missing!");
 
-    public static readonly bool RecreatingDb = true;
+    public const bool RecreatingDb = true;
     
-    public static readonly string PathToInventoryXlsxFiles = ConfigurationManager.AppSettings.Get("PathToInventoryXlsxFiles")
+    private static readonly string PathToInventoryXlsxFiles = ConfigurationManager.AppSettings.Get("PathToInventoryXlsxFiles")
                                                     ?? throw new Exception("Path to xlsx files is missing!");
 
-    public static readonly int WorkSheetNum = 0;
+    public const int WorkSheetNum = 0;
 
     public static readonly (int Row, int Col) InformationSystemCodeRowCol = (0, 4);
     public static readonly (int Row, int Col) InformationSystemNameRowCol = (1, 4);
@@ -32,33 +31,33 @@ public static class Settings
 
     public const string DomainSearchWord = "vaz";
 
-    public static readonly int ServerDomainCol = 2;
+    public const int ServerDomainCol = 2;
 
-    public static readonly int ServerOsNameCol = 3;
-    public static readonly int ServerOsVersionCol = 4;
+    public const int ServerOsNameCol = 3;
+    public const int ServerOsVersionCol = 4;
 
-    public static readonly int ServerApplicationNameCol = 5;
-    public static readonly int ServerApplicationVersionCol = 6;
+    public const int ServerApplicationNameCol = 5;
+    public const int ServerApplicationVersionCol = 6;
 
-    public static readonly int ServerLocationCol = 7;
+    public const int ServerLocationCol = 7;
     
     // ________________________________________________________________________________________________
 
     public static readonly string DbLanitDSN = ConfigurationManager.AppSettings.Get("DbLanitDSN")
                                                ?? throw new Exception("Db Lanit connection string is missing!");
     
-    public static readonly int LanitCodeCol = 0;
-    public static readonly int LanitSystemNameCol = 1;
-    public static readonly int LanitDomainCol = 3;
-    public static readonly int LanitServersKindCol = 4;
-    public static readonly int LanitIntegrationsCol = 5;
+    public const int LanitCodeCol = 0;
+    public const int LanitSystemNameCol = 1;
+    public const int LanitDomainCol = 3;
+    public const int LanitServersKindCol = 4;
+    public const int LanitIntegrationsCol = 5;
 
-    public static readonly string PathToLanitXlsxFile = ConfigurationManager.AppSettings.Get("PathToLanitXlsxFile")
+    private static readonly string PathToLanitXlsxFile = ConfigurationManager.AppSettings.Get("PathToLanitXlsxFile")
                                                         ?? throw new Exception("Path to Lanit xlsx file is missing!");
 
-    public static readonly string LanitFileName = "LanitCompare.xlsx";
+    public const string LanitFileName = "LanitCompare.xlsx";
     
-    public static readonly int StartRow = 1;
+    public const int StartRow = 1;
 
     public static readonly List<string> DirectoriesForParse = new()
         { PathToInventoryXlsxFiles, PathToLanitXlsxFile };
