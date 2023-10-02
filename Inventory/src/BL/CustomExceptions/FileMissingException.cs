607b588a7b0ddb90fe48f2e18440c9de465fd3e3
Harlok <holmik90@yandex.ru>
2023-10-02 21:15:45 +0400

    feat: add class FileMissingException

--- Inventory/src/BL/CustomExceptions/FileMissingException.cs
+++ Inventory/src/BL/CustomExceptions/FileMissingException.cs
@@ -4,7 +4,9 @@ public class FileMissingException : ApplicationException
 {
     public DateTime ErrorTimeStamp { get; set; }
 
-    public FileMissingException(string message, DateTime time) : base(message)
+    public FileMissingException(string message, DateTime time) : this(message, time, null) { }
+
+    public FileMissingException(string message, DateTime time, Exception? inner) : base(message, inner)
     {
         ErrorTimeStamp = time;
     }
