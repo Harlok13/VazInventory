cc0fec69b68a0a8b44a8ca4ab37105db6c911253
Harlok <holmik90@yandex.ru>
2023-10-02 21:11:55 +0400

    fix: fix type of context

--- Inventory/src/BL/ExcelProcessing/IXlsxProcessing.cs
+++ Inventory/src/BL/ExcelProcessing/IXlsxProcessing.cs
@@ -1,10 +1,11 @@
+using Microsoft.EntityFrameworkCore;
+
 using Inventory.DAL.Cache;
-using Inventory.DAL.Context;
 using Inventory.DAL.ExcelToDatabase;
 
 namespace Inventory.BL.ExcelProcessing;
 
 public interface IXlsxProcessing
 {
-    public Task StartAsync(InventoryContext context, IExcelToDatabase excelToDatabase, ICacheData cacheData);
+    public Task StartAsync(DbContext context, IExcelToDatabase excelToDatabase, ICacheData cacheData);
 }
